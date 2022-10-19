ce31fafc89828df08cc326a04c60032c4dc78e54
TommyDumora <tommydumora@gmail.com>
2022-10-19 17:37:09 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, dolore accusamus! Rem quas tempore ut iste itaque obcaecati quasi error magnam deleniti, dignissimos officia inventore consectetur similique perspiciatis, deserunt explicabo dolore nobis nesciunt sunt earum illum ea, totam consequatur? Ratione, minus atque reprehenderit optio et incidunt exercitationem recusandae. Velit quis, consequuntur eligendi id odit natus animi molestias sequi laudantium cum quae vitae accusantium, debitis placeat optio dignissimos, cupiditate nostrum est atque laboriosam cumque iste. Maxime quis dolorem autem minima sapiente tempore sint ut praesentium et necessitatibus sequi quaerat consequatur voluptates quisquam maiores accusamus repudiandae reiciendis unde, alias voluptatem cupiditate odio.</p>
+            <br />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque beatae reiciendis ut tempore tempora illum vitae, odio omnis nulla eligendi veritatis repellat dolorum earum mollitia quisquam saepe eum dolores unde facilis iure quidem natus facere veniam. Tempore placeat officia explicabo veritatis aliquam accusantium tempora, veniam odit blanditiis ex! Aliquid sunt eaque enim amet mollitia, illum, dolorem consectetur nam, eligendi tempore impedit explicabo molestiae asperiores! Asperiores officiis ut tenetur! Praesentium blanditiis placeat dolor, eos ullam mollitia minima. Ipsum minima delectus dolorum! Dolorem quasi corporis accusamus delectus ea. Doloribus, sed sit reprehenderit odio tempore unde, libero architecto deleniti iusto eius consectetur nam repudiandae doloremque, illo quasi et. Nihil, dolor eveniet, sunt, repellendus perferendis in vitae facere voluptate facilis rerum neque suscipit! Minima ut non quaerat, eos molestias, provident dignissimos error nobis odio, ipsam dolorem labore. Molestias voluptate sunt earum modi optio esse deleniti! Doloremque eaque deserunt ipsam mollitia, quia dolorem eos qui voluptates, nemo ea fugit? Voluptate sequi ad magnam? Itaque mollitia, doloremque dolore, nulla placeat inventore, eius suscipit eum modi fugiat fugit repellat voluptatum impedit sunt reprehenderit. Quaerat atque asperiores, eum eaque ipsam quae obcaecati ullam voluptate similique nemo tempora? Incidunt a perspiciatis, officia magnam aspernatur perferendis atque sunt eos error.</p>
+        </div>
+    );
+};
+
+export default About;
